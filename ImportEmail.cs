edc242e5d35c95936def26487fbc3e14b0067466
Dennis Krupitsky <d.krupitsky@gmail.com>
2020-02-27 14:46:07 -0500

    beginning development, also have basic backend code that needs alteration

---
+++ ImportEmail.cs
@@ -0,0 +1,80 @@
+﻿//headers
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Outlook;
+using TranslateTextSample; 
+
+
+namespace ConsoleApp1
+{
+    class ImportEmail
+    {
+        //Mutators/accessors
+        public string EmailFrom { get; set; }
+        public string EmailSubject { get; set; }
+        public string EmailBody  { get; set; }
+
+        //List to read in the emails
+        public static List<ImportEmail> ReadEmailItem()
+        {
+            //create the connection
+            Application outlookApplication = null;
+            NameSpace outlookNameSpace = null;
+            MAPIFolder inboxFolder = null;
+            Items mailItems = null;
+
+            //list of email items
+            List<ImportEmail> ListEmailDetials = new List<ImportEmail>();
+            ImportEmail emailDetails;
+
+            //attempt to extract the emails
+            try
+            {
+                outlookApplication = new Application();
+                outlookNameSpace = outlookApplication.GetNamespace("MAPI");
+                inboxFolder = outlookNameSpace.GetDefaultFolder(OlDefaultFolders.olFolderInbox);
+                mailItems = inboxFolder.Items;
+                foreach (MailItem mail in mailItems)
+                {
+                    emailDetails = new ImportEmail();
+                    emailDetails.EmailFrom = mail.SenderEmailAddress;
+                    emailDetails.EmailSubject = mail.Subject;
+                    emailDetails.EmailBody = mail.Body;
+                    ListEmailDetials.Add(emailDetails);
+                    ReleaseObject(mail);
+                }
+            }
+            catch (System.Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            finally
+            {
+                ReleaseObject(mailItems);
+                ReleaseObject(inboxFolder);
+                ReleaseObject(outlookNameSpace);
+                ReleaseObject(outlookApplication);
+            }
+            
+
+            return ListEmailDetials;
+
+        }
+        //return the mail object
+            private static void ReleaseObject(object obj)
+            {
+                if (obj != null)
+                {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+                }
+            }
+    }
+}
+
+
+
+
---
+++ Main.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using ConsoleApp1;
+using TranslateTextSample;
+namespace Email_Translate
+{
+    class Driver
+    {
+        static async Task Main(string[] args)
+        {
+
+            var mails = ImportEmail.ReadEmailItem();
+            //keeps track of the email number 
+           
+
+
+
+           
+            //loop to output details about email contents
+            /*
+            foreach (var item in mails)
+            {
+                Console.WriteLine("Mail No" + i);
+                Console.WriteLine("Mail Recieved From: " + item.EmailFrom);
+                Console.WriteLine("Mail Subject: " + item.EmailSubject);
+                Console.WriteLine("Mail Body: " + item.EmailBody);
+                Console.WriteLine(" ");
+                i++;
+
+            }
+            */
+
+           
+
+
+            
+            //set string variable
+            string path = @"C:/Users/denni/Documents/Intern-Challenge UBS 2019/Email-Translation/Email-Translate/TextOutput.txt";
+            string host = "https://api.cognitive.microsofttranslator.com";
+            string route = "/translate?api-version=3.0&to=en";
+            string subscriptionKey = "5f9c84b31e2a4b19bd277785264c53ff";
+
+
+            string statement = "            TRANSLATION OF 5 EMAILS IN DIFFERENT LANGUAGES" + "\n" +
+            "-----------------------------------------------------------------------------------------" + "\n";
+
+            // output to a txt file
+            System.IO.File.WriteAllText(path, statement);
+
+
+            //int for controlling mail item
+            int i = 1;
+            //Access mail item
+
+
+
+            for (int x = 971; x <= 975; x++)
+            {
+                var item = mails[x];
+                string textToTranslate = item.EmailBody;
+                //string variables
+                string output = "Email " + i + ": " + textToTranslate + "\n" + " -------------------------------------------------" + "\n";
+                System.IO.File.AppendAllText(path, output);
+                await TranslateTextSample.Program.TranslateTextRequest(subscriptionKey, host, route, textToTranslate);
+                i++;
+            }
+
+
+            Console.WriteLine("Complete");
+            Console.ReadKey();
+           
+
+            
+
+        }
+    }
+}
